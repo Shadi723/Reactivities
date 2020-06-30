e29ba77ecda2f92678777e8091085ec4f6c0b3f3
Shadi723 <39144884+Shadi723@users.noreply.github.com>
2020-07-01 02:16:17 +0300

    initialCommet

---
+++ API/Controllers/DemoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Presistance;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DemoController : ControllerBase
+    {
+        private readonly DataContext _db;
+
+        public DemoController(DataContext data)
+        {
+            _db = data;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _db.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int? id)
+        {
+            var value = await _db.Values.FirstOrDefaultAsync((e)=>e.Id == id);
+            return Ok(value);
+        }
+    }
+}
